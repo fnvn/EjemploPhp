175b00700d32eedd593027b612cd11143759f9aa
fnvn <mshitcake@gmail.com>
2019-08-28 23:41:12 -0300

    var_dump trabajamos en la pag1

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<?php
+$nombre = "Línea Chocman";
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tilin Tilin</title>
+    </head>
+    <body>
+        <h1 align="center">Ingreso de Productos </h1>
+        <?php
+        // put your code here
+        echo "<h1 align=center>" . $nombre . "</h1>";
+        ?>
+
+        <form action="pag1.php" method="post">
+            <label> Codigo: </label>
+            <input type="text" name="codigo"/> <br>
+           
+
+                <label> Marca: </label>
+                <input type="text" name="marca"/>  <br>
+                    <label> Modelo: </label>
+                    <input type="text" name="modelo"/>  <br>
+
+                        <label> Precio: </label>
+                        <input type="number" name="precio"/>  <br>
+                      
+                            <input type="submit" value="registrar"/> 
+
+
+                                </form>
+                                </body>
+                                </html>
---
+++ pag1.php
@@ -0,0 +1,11 @@
+<?php
+
+var_dump($_POST);
+$nombre=$_POST['nombre'];
+echo $nombre;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
