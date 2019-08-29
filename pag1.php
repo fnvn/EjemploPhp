fce9f13d7b6fda9c03ec98b7dfdb815feabad425
fnvn <mshitcake@gmail.com>
2019-08-29 00:03:03 -0300

    Estilo en cascada

--- pag1.php
+++ pag1.php
@@ -1,8 +1,8 @@
 <?php
 
 var_dump($_POST);
-$nombre=$_POST['nombre'];
-echo $nombre;
+$marca=$_POST['marca'];
+echo $marca;
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
